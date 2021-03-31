7573009dc15e4591dad7a8f17f3549978f899196
Anthony Fu <anthonyfu117@hotmail.com>
2021-03-31 19:56:49 +0800

    feat: add demo note

---
+++ src/components/DemoNote.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="-mt-10 mb-12 rounded px-4 py-2 text-primary bg-primary bg-opacity-5 space-x-1">
+    💡 <span class="opacity-75">This is a demo of Windi Analyzer, check out the</span>
+    <a
+      href="https://github.com/windicss/windicss-analysis"
+      target="_blank"
+      class="cursor-pointer border-b border-dashed border-current hover:(border-solid)"
+    >GitHub repo</a>
+    <span class="opacity-75">for instructions to run it with your projects.</span>
+  </div>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+import { isDemo } from '~/env'
+</script>
+
 <template>
   <div class="container">
+    <DemoNote v-if="isDemo" />
     <Overview class="pb-2" />
     <TopUtilities class="pb-2" />
     <ColorPalette class="pb-2" />
